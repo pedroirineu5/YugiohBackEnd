ec35126e0181d02eec99e5a1e3f1a880ece07c10
pedroirineu5 <pedrohenriqueirineu@gmail.com>
2026-04-20 02:34:29 -0300

    feat: add serializer for Card model to handle data representation

---
+++ cards/serializers.py
@@ -0,0 +1,9 @@
+from dataclasses import field
+from rest_framework import serializers
+from cards.models import Card
+
+
+class CardsSerializers(serializers.ModelSerializer):
+    class Meta:
+        model = Card
+        field = '__all__'
